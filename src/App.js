a8ed9bf3678a45517eadb3c20eb098a5ffa8f9c8
Evgeny Chernov <e.chernov@lucky-labs.com>
2022-06-24 16:09:37 +0300

    fix endpoint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import socketIOClient from "socket.io-client";
 import Board from './Board';
-const ENDPOINT = "http://127.0.0.1:4001";
+const ENDPOINT = "'https://frozen-island-70548.herokuapp.com':4001";
 
 const App = () => {
     const [response, setResponse] = useState("");
